4f1a7cab1f38b7c4f7ed45ebd813fbb23869c5a0
Andres Romero <aromerosambucetti@gmail.com>
2024-09-11 00:07:28 -0300

    carousel de destacados

--- src/components/Home/Promoted.jsx
+++ src/components/Home/Promoted.jsx
@@ -14,28 +14,96 @@ import 'swiper/css';
 
 
 const Promoted = () => {
-  
+  const products = [
+    { 
+      image: '/images/casa-1.png',
+      type: 'alquiler', 
+      title: 'Punta del Este, Maldonado', 
+      description: '6 Dormitorios • 4 baños • 350m2',
+      url: '/catalog',
+      price: 'U$s 4000'
+    },
+    { 
+      image: '/images/casa-1.png',
+      type: 'alquiler', 
+      title: 'Punta del Este, Maldonado', 
+      description: '6 Dormitorios • 4 baños • 350m2',
+      url: '/catalog',
+      price: 'U$s 4000'},
+    { 
+      image: '/images/casa-1.png',
+      type: 'alquiler', 
+      title: 'Punta del Este, Maldonado', 
+      description: '6 Dormitorios • 4 baños • 350m2',
+      url: '/catalog',
+      price: 'U$s 4000'
+    },
+    { 
+      image: '/images/casa-1.png',
+      type: 'alquiler', 
+      title: 'Punta del Este, Maldonado', 
+      description: '6 Dormitorios • 4 baños • 350m2',
+      url: '/catalog',
+      price: 'U$s 4000'
+    },
+  { 
+    image: '/images/casa-1.png',
+    type: 'alquiler', 
+    title: 'Punta del Este, Maldonado', 
+    description: '6 Dormitorios • 4 baños • 350m2',
+    url: '/catalog',
+    price: 'U$s 4000'
+  },
+  { 
+    image: '/images/casa-1.png',
+    type: 'alquiler', 
+    title: 'Punta del Este, Maldonado', 
+    description: '6 Dormitorios • 4 baños • 350m2',
+    url: '/catalog',
+    price: 'U$s 4000'},
+      
+  ];
+
     return (
         <section className='home_promoted'>
+       <h2 className='home_promoted-title'>Propiedades destacadas</h2>
+       <p className='home_promoted-text'>Conoce las propiedades más destacadas del mercado.</p>
+        <article className='home_promoted-slider'>
               <Swiper
-        slidesPerView={4.5}
-        spaceBetween={30}
+        slidesPerView={5}
+        spaceBetween={5}
         pagination={{
           clickable: true,
         }}
        
         className="mySwiper"
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
+         {products.map((product, index) =>( 
+           <SwiperSlide>
+           <div className='promoted_card' key={index}>
+             <div className='promoted_card_image'>
+               <img src={product.image} alt="" />
+             </div>
+             <h4>{product.title}</h4>
+             <p>{product.description}</p>
+             <p>{product.price}<span>{product.type}</span></p>
+             <button className='promoted_button'>Más info</button>
+           </div>
+         </SwiperSlide>
+          ))}
+        <SwiperSlide>
+          <div className='promoted_card' >
+            <div className='promoted_card_image'>
+              <img src="/images/casa-1.png" alt="" />
+            </div>
+            <h4>Punta del Este, Maldonado</h4>
+            <p>6 Dormitorios • 4 baños • 350m2 </p>
+            <p>U$s 4000 mes</p>
+            <button className='promoted_button'>Más info</button>
+          </div>
+        </SwiperSlide>
       </Swiper>
+        </article>
         </section>
     );
 };
