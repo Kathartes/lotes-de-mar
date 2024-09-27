ba5a4af5b914d6a9b691f263c76bef20d5657670
Andres Romero <aromerosambucetti@gmail.com>
2024-09-27 13:24:48 -0300

    agregado de posteo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home'
 import Property from './components/property';
 import Catalog from './components/Catalog';
 import Plan from './components/Plan';
+import Post from './components/Post';
 import './App.css'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
         <Route path="/property" element={<Property />} />
         <Route path="/catalog" element={<Catalog />} />
         <Route path="/plan" element={<Plan/>} />
+        <Route path="/post" element={<Post/>} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/components/Post.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Post.css';
+
+
+
+
+
+const Post = () => {
+  
+    return (
+        <>
+        <header className='home_header'>
+            <div className='home_logo'>
+                <img src="/icons/whale.png" alt="" />
+                <p className='home_logo-title'>Lotesde<span>mar</span></p>
+            </div>
+            <div className='home_login'>
+                <img src="/icons/user.png" alt="" />
+            </div>
+        </header>
+        <form action="">
+            <h1>¡Empezá a publicar hoy!</h1>
+            <p>continúa los siguientes pasos y en menos de 3 minutos publica tu inmueble</p>
+            <fieldset>
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+                <input type="file" />
+            </fieldset>
+            <fieldset>
+                <legend>CUÁL ES EL TÍTULO DE LA PUBLICACIÓN</legend>
+                <input type="text" />
+            </fieldset>
+            <fieldset>
+                <legend>DESCRIBE TU INMUEBLE</legend>
+                <textarea name="" id=""></textarea>
+            </fieldset>
+            <fieldset>
+                <legend>Complete la siguiente información</legend>
+                <input type="checkbox" name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+                <input type="checkbox"  name="" id="" />
+            </fieldset>
+            <fieldset>
+                <legend>PRECIO DEL INMUEBLE</legend>
+                <input type="number" />
+            </fieldset>
+            <fieldset>
+                <legend>Complete la dirección del inmueble</legend>
+                <input type="text" />
+            </fieldset>
+            <fieldset>
+                <legend>Número de contacto de la inmobiliaria</legend>
+                <input type="number" />
+            </fieldset>
+            <button type='submit'>CONTINUAR</button>
+        </form>
+        
+        </>
+    );
+};
+
+export default Post;
